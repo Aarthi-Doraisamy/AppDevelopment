e273bbd3b28bbe5941454ca503ce094d7b89f958
Aarthi-Doraisamy <110532469+Aarthi-Doraisamy@users.noreply.github.com>
2023-10-16 12:37:53 +0530

    Delete Day 22 directory
     dont need

--- Day 22/src/main/java/com/repository/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, String>{
-
-}
--- Day 22/src/main/java/com/repository/LibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Library;
-
-public interface LibraryRepository extends JpaRepository<Library, Integer>{
-	
-}
